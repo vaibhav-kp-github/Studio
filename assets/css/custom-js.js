c6bbed0e1c4728e53328f4626596774108ab5669
vaibhav-kp-github <guptavaibhav14092001@gmail.com>
2024-04-25 22:21:41 +0530

    first

---
+++ assets/css/custom-js.js
@@ -0,0 +1,12 @@
+function hideshow() {
+    if(document.getElementById("hide-show-hamburger-section").style.display =="flex") {
+        document.getElementById("hide-show-hamburger-section").style.display ="none";
+    }else{document.getElementById("hide-show-hamburger-section").style.display = "flex";            
+        }    
+    if(document.getElementById("navhide").style.opacity =="0") {
+        document.getElementById("navhide").style.opacity ="1";
+    }else{document.getElementById("navhide").style.opacity = "0";            
+        }    
+    
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html",
+            "./dist/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
